a352f3fe250646651f52e9f90c505ed3cfa39d6e
AkewakBiru <akawaq1234@gmail.com>
2023-08-27 20:04:19 +0400

    fixed some stuff and added documentation

--- Commands.cpp
+++ Commands.cpp
@@ -6,7 +6,7 @@
 /*   By: abiru <abiru@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 11:55:48 by abiru             #+#    #+#             */
-/*   Updated: 2023/08/20 16:53:49 by abiru            ###   ########.fr       */
+/*   Updated: 2023/08/27 19:52:15 by abiru            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,13 @@ bool PASS(Server &server, Client *client, std::vector<std::string> const &res)
 	return (true);
 }
 
+/*
+	** sets nick for the user
+		- if nick doesn't have any arg, sends ERR_NONICKNAMEGIVEN
+		- if nick is already taken, sends ERR_NICKNAMEINUSE
+		- if nick has more than 9 chars, sends ERR_TOOLONGNICKNAME
+		- if nick has illegal chars, sends ERR_ERRONEUSNICKNAME
+*/
 bool NICK(Server &server, Client *client, std::vector<std::string> const &res)
 {
 	std::string tmp;
@@ -49,9 +56,17 @@ bool NICK(Server &server, Client *client, std::vector<std::string> const &res)
 	return (true);
 }
 
+/*
+	** sets username of the new user:
+		- if user is already registered, sends ERR_ALREADYREGISTRED error msg
+		- if user isn't registered, and command sent doesn't have enough params,
+			sends ERR_NEEDMOREPARAMS error msg
+*/
 bool USER(Server &server, Client *client, std::vector<std::string> const &res)
 {
 	(void) server;
+	if (client->getStatus())
+		throw std::runtime_error(genErrMsg(ERR_ALREADYREGISTRED, client->getNick(), "", ERR_ALREADYREGISTRED_DESC));
 	if (res.size() < 6)
 		throw std::invalid_argument(genErrMsg(ERR_NEEDMOREPARAMS, "*", res[1], ERR_NEEDMOREPARAMS_DESC));
 	else if (userIllegalChars(res[2]))
@@ -66,6 +81,9 @@ static bool isValidOption(std::string const &arg)
 	return (arg == "LS" || arg == "REQ" || arg == "ACK" || arg == "END" || arg == "LIST");
 }
 
+/*
+	** sends CAP negotiation reply to a client
+*/
 bool CAP(Server &server, Client *client, std::vector<std::string> const &res)
 {
 	(void) server;
@@ -77,6 +95,8 @@ bool CAP(Server &server, Client *client, std::vector<std::string> const &res)
 		throw std::invalid_argument(genErrMsg(ERR_INVALIDCAPCMD, "*", res[2], ERR_INVALIDCAPCMD_DESC));
 	else if (res[2] == "LS" && (resSize == 3 || resSize == 4))
 		sendMsg(client->getFd(), "CAP * LS :multi-prefix\r\n");
+	// request to a change in capability is replied with an ACK
+	// containing the list of capabilities that a client can do
 	else if (res[2] == "REQ" && resSize >= 4)
 		sendMsg(client->getFd(), "CAP * ACK :multi-prefix\r\n");
 	return (true);
@@ -92,36 +112,30 @@ bool MOTD(Server &server, Client *client, std::vector<std::string> const &res)
 	std::stringstream buf;
 	std::string strBuf;
 	size_t end = 0;
-	
-	file.open("MOTD", std::ios::in);
+
+	file.open("MOTD.conf", std::ios::in);
+	if (!file.is_open())
+		throw std::invalid_argument(genErrMsg(ERR_NOMOTD, "*", "*", ERR_NOMOTD_DESC));
 	if (res.size() == 3 && res[2] != "ircserv")
 	{
 		file.close();
 		throw std::invalid_argument(genErrMsg(ERR_NOSUCHSERVER, client->getNick(), res[2], ERR_NOSUCHSERVER_DESC));
 	}
-	else if (!file.is_open())
-		throw std::invalid_argument(genErrMsg(ERR_NOMOTD, "*", "*", ERR_NOMOTD_DESC));
 	buf << file.rdbuf();
 	file.close();
 	strBuf = buf.str();
 	sendMsg(client->getFd(), genErrMsg(RPL_MOTDSTART, client->getNick(), "", RPL_MOTDSTART_DESC));
-	while (true)
+	while ( (end = strBuf.find('\n')) != std::string::npos )
 	{
-		end = strBuf.find('\n');
-		if (end == std::string::npos)
-			break ;
-		else
+		if (end > 80)
 		{
-			if (end > 80)
-			{
-				strBuf.erase(0, end + 1);
-				continue ;
-			}
-			sendMsg(client->getFd(), genErrMsg(RPL_MOTD, client->getNick(), ":-", strBuf.substr(0, end)));
 			strBuf.erase(0, end + 1);
+			continue ;
 		}
+		sendMsg(client->getFd(), genErrMsg(RPL_MOTD, client->getNick(), ":-", strBuf.substr(0, end)));
+		strBuf.erase(0, end + 1);
 	}
-	if (strBuf.size() && strBuf.size() <= 90)
+	if (strBuf.size() && strBuf.size() <= 80)
 		sendMsg(client->getFd(), genErrMsg(RPL_MOTD, client->getNick(), ":-", strBuf.substr(0, strBuf.size())));
 	sendMsg(client->getFd(), genErrMsg(RPL_ENDOFMOTD, client->getNick(), "", RPL_ENDOFMOTD_DESC));
 	return (true);
