3ea80d8e2c4a72a0c5d27aa90fd2e94cf07a980e
shatha-jad <110840047+shatha-jad@users.noreply.github.com>
2023-09-17 11:06:11 +0000

    commands and mods

---
+++ channel_commands.hpp
@@ -0,0 +1,25 @@
+
+#ifndef CHANNEL_COMMANDS_HPP
+#define CHANNEL_COMMANDS_HPP
+#include "Channel.hpp"
+#include "Client.hpp"
+#include <vector>
+#include <string>
+
+
+class Client;
+
+class ChannelCommands
+{
+public:
+    ChannelCommands();
+    ~ChannelCommands();
+
+    void invite(Channel& channel, Client& inviter, Client& invitee);
+    void kick(Channel& channel, Client& kicker, Client& kickee);
+    void setTopic(Channel& channel, Client& client, const std::vector<std::string>& messages);
+    void manageMods(Channel& channel, Client& client, const std::vector<std::string>& messages);
+
+};
+
+#endif
