762d828e5574764ecd2f1479cc8d51ea19499361
Youssef <youssefeltoukhi@hotmail.com>
2023-09-24 15:49:51 +0400

    Added funtion prototypes

--- Commands.hpp
+++ Commands.hpp
@@ -22,4 +22,11 @@ bool PRIVMSG(Server &server, Client *client, std::vector<std::string> const &res
 bool JOIN(Server &server, Client *client, std::vector<std::string> const &res);
 bool QUIT(Server &server, Client *client, std::vector<std::string> const &res);
 bool NAMES(Server &server, Client *client, Channel *channel);
+
+
+void INVITE(Server &server, Client *client, std::vector<std::string> const &res);
+void KICK(Server &server, Client *client, std::vector<std::string> const &res);
+void setTopic(Server &server, Client *client, std::vector<std::string> const &res);
+void manageMods(Server &server, Client *client, std::vector<std::string> const &res);
+void executeCommand(Server &server, Client *client, std::vector<std::string> const &res);
 #endif
